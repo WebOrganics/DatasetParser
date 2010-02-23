4198fd479dd3a61e3e9103e06f9e7680eb032ca4
Martin McEvoy <martin@weborganics.co.uk>
2010-02-23 01:19:31 +0000

    Tidy code bump version to 0.2

--- example.php
+++ example.php
@@ -1,10 +1,13 @@
 <?php
-#require DatasetParse class,
-require_once( 'datasetParser.php' );
+#require Dataset.parser class,
+require_once( 'dataset.parser.php' );
 
 #define a $url in this case get url, ?url=http://foo.com/dataset.bar 
 $url = $_GET['url'];
 
+#start a new HTMLQuery
+$query = new HTMLQuery;
+
 #echo, or print results
-echo DatasetParse::html_query($url);
+echo $query->this_document($url);
 ?>
