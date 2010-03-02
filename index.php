a261c25ba0bb9946d84c1ccbe5067c91b9281a2e
Martin McEvoy <martin@weborganics.co.uk>
2010-03-02 17:05:50 +0000

    Replaced example.php for index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include_once( 'application/Dataset_Parser.php' );
 #start a new HTMLQuery
 $query = new HTMLQuery;
 
-#define a $query in this case get url, example.php?query=http://foo.com/dataset.bar 
+#define a $query in this case get url, index.php?query=http://foo.com/dataset.bar 
 $url = $_GET['query'];
 
 #echo, or print results
